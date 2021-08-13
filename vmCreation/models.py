fd761643a9c1d0265c794e10660d0203e860fa87
amnik <amniksec@gmail.com>
2021-08-13 13:42:31 +0000

    Add database model

--- vmCreation/models.py
+++ vmCreation/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Server(models.Model):
+    name = models.CharField(max_length = 254,blank=True, null=True)
+    ram = models.CharField(max_length = 2,blank=True, null=True)
+    cpu = models.CharField(max_length = 2,blank=True, null=True)
+    storage = models.CharField(max_length = 5,blank=True, null=True)
