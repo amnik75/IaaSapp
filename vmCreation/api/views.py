b36cbe8c6af74c91b5ab2ae851d974cc09215910
amnik <amniksec@gmail.com>
2021-08-14 18:18:06 +0000

    Use database for vm creation

--- vmCreation/api/views.py
+++ vmCreation/api/views.py
@@ -13,9 +13,18 @@ class Create(APIView):
         virtips = ['192.168.122.104','192.168.122.119']
         if serializer.is_valid():
             ip = virtips[randrange(2)]
-            print(ip)
-            result = subprocess.run(['ssh','192.168.122.119','sudo','./vmCreate.sh',request.data['name'],str(int(request.data['ram']) * 1024),request.data['cpu'],request.data['storage']],stdout=PIPE, stderr=PIPE)
-            print(result.stdout.decode())
+            result = subprocess.run(['ssh',ip,'sudo','./vmCreate.sh',request.data['name'],str(int(request.data['ram']) * 1024),request.data['cpu'],request.data['storage']],stdout=PIPE, stderr=PIPE)
+            serializer.save()
             return Response(result.stdout.decode(), status=status.HTTP_200_OK)
-            
+        elif isRepeating(request.data) :
+            return Response("The name is used by an other customer", status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_200_OK)
 
+def isRepeating(data):
+    if "name" in data:
+        name = data["name"]
+        servers = Server.objects.all()
+        for server in servers:
+            if server.name == name:
+                return True
+    return False
