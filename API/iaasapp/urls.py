20ff04e0baf30ec4d9539809771b56632d6c9a3c
amnik <amniksec@gmail.com>
2021-08-17 14:03:44 +0000

    update urls

--- API/iaasapp/urls.py
+++ API/iaasapp/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('', include('vmCreation.urls')),
     url('api/', include('vmCreation.api.urls')),
+    url('', include('vmCreation.urls')),
 ]
